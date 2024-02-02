a9bc8bc725784ff8512ab7eea0db688e926d414c
unknown <kbchung2@illinois.edu>
2024-02-01 21:58:04 -0600

    Passed test baseline

--- mp03/submitted.py
+++ mp03/submitted.py
@@ -17,6 +17,8 @@ import numpy as np
 
 # define your epsilon for laplace smoothing here
 
+epsilon = 0.05 # is this a reasonable value?
+
 def baseline(test, train):
     '''
     Implementation for the baseline tagger.
@@ -25,7 +27,31 @@ def baseline(test, train):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+    most_frequent_tag = ""
+    highest_tag_count = 0
+    most_frequent_tag_to_highest_tag_count = Counter()
+    wordToTagCount = {}
+    output = []
+    for sentence in train:
+        for word,tag in sentence:
+                if word not in wordToTagCount:
+                        wordToTagCount[word] = Counter()
+                wordToTagCount[word][tag] += 1
+                most_frequent_tag_to_highest_tag_count[tag] += 1
+                if wordToTagCount[word][tag] > highest_tag_count:
+                        highest_tag_count = wordToTagCount[word][tag]
+                        most_frequent_tag = tag
+#     print(wordToTagCount)
+    for sentence in test:
+            sentence_list = []
+            for word in sentence:
+                    if word not in wordToTagCount:
+                            sentence_list.append((word, most_frequent_tag_to_highest_tag_count.most_common(1)[0][0]))
+                    else:
+                            frequent_tag = wordToTagCount[word].most_common(1)[0][0]
+                            sentence_list.append((word,frequent_tag))
+            output.append(sentence_list)
+    return output
 
 
 def viterbi(test, train):
@@ -36,8 +62,8 @@ def viterbi(test, train):
     output: list of sentences with tags on the words
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
-
+    output = []
+    return output
 
 def viterbi_ec(test, train):
     '''
@@ -47,7 +73,6 @@ def viterbi_ec(test, train):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
 
 
 
